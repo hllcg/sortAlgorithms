7e800c7b3fa9ec9aec93d02383ab3ac1215c0106
hllcg <halilcaglar26@hotmail.com>
2017-10-10 14:19:30 +0300

    sort headers added

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include "linearsort.h"
 #include "bubleSort.h"
-//TODO BucketSort  (LinearSort)
-//TODO RadixSort (LinearSort)
-//TODO 
+//1-) BubleSort
+//2-) BucketSort  (LinearSort)
+//3-) RadixSort (LinearSort)
+//4-) InsertionSort
+//5-) MergeSort
+//6-) QuickSort
+//7-) HeapSort
+//8-) SelectionSort
+
 int main(int argc, char **argv)
 {
 //   std::vector<unsigned char> vec{1,2,8,24,5,2,3,1,2};
   Data<unsigned char> vec;
-  vec.randomSet(3);
+  vec.randomSet(24);
   BubleSort<unsigned char> bubleSort(vec);
   std::cout<<bubleSort;
   return 0;
